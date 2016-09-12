7e96b4e91b736e84de5bc87cadb05376e65af95e
105021034@live.asia.edu.tw <jason565>
2016-09-12 15:58:06 +0800

    HK1

---
+++ DanTin123/src/HK01.java
@@ -0,0 +1,18 @@
+
+public class HK01 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("我叫周宇晟");
+		System.out.println("我來自台南");
+		System.out.println("我射手座");
+		System.out.println("我喜歡天空藍");
+		System.out.println("我喜歡到處跑");
+		System.out.println("我想試試熱舞社");
+		System.out.println("我想當駭客");
+	}
+
+}
